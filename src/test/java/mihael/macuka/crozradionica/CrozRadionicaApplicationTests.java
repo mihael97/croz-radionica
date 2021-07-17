79bd3514ed93d4e3c12ef30197c8e407ef13e604
Mihael <mihaelmacuka2@gmail.com>
2021-07-17 19:11:06 +0200

    Initial commit

---
+++ src/test/java/mihael/macuka/crozradionica/CrozRadionicaApplicationTests.java
@@ -0,0 +1,13 @@
+package mihael.macuka.crozradionica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrozRadionicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
